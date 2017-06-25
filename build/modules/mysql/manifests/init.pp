1a49b7f62b66256e588e345d76161bb05cf3726f
Joao Afonso <joao.afonso@bab-oon.com>
2017-06-25 13:51:58 +0100

    Mysql is now properly loaded

--- build/modules/mysql/manifests/init.pp
+++ build/modules/mysql/manifests/init.pp
@@ -2,6 +2,9 @@ class mysql {
 
   # root mysql password
   $mysqlpw = "d3v0p5"
+  $user = "baboon_user"
+  $password = "an055apa55"
+  $db = "baboon_mondaysoccer"
 
   # install mysql server
   package { "mysql-server":
@@ -20,5 +23,16 @@ class mysql {
     unless => "mysqladmin -uroot -p$mysqlpw status",
     command => "mysqladmin -uroot password $mysqlpw",
     require => Service["mysql"],
+  }
+
+  exec { "create-database":
+    unless => "mysql -u$user -p$password $db",
+    command => "mysql -uroot -p$mysqlpw -e \"create database $db; grant all on $db.* to $user@localhost identified by '$password';\"",
+    require => [Service["mysql"], Exec["set-mysql-password"]],
+  }
+
+  exec { "import mysql":
+    command => "mysql -u $user -p$password $db < /home/vagrant/mondaysoccer_ws/database/mondaysoccer.sql",
+    require => [Service["mysql"], Exec["create-database"]],
   }
 }
